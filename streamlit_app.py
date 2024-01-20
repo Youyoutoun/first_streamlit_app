df66cfc90a0c73beda2406300587eba19441793b
Youyoutoun <157043847+Youyoutoun@users.noreply.github.com>
2024-01-20 21:27:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,10 +92,24 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 ###
-streamlit.stop()
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('thank for adding '+add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
+##
+#streamlit.stop()
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+#streamlit.write('thank for adding '+add_my_fruit)
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+##
+
+###
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "thank for adding "+add_my_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.screts["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+###
 
 
